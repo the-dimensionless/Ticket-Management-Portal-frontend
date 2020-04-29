310d16957d43c277cb5b1743f7f1dcc648f300be
Sumit Singh <sumit.singh@nagarro.com>
2020-04-29 19:45:03 +0530

    Review changes form

--- src/app/user-reg-form/user-reg-form.component.ts
+++ src/app/user-reg-form/user-reg-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-reg-form',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegFormComponent implements OnInit {
 
+  public showReview: boolean;
+  public submitter: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    // this.showReview = false;
+    // this.submitter = true;
+    document.getElementById("reviewContainer").style.display = "none";
+    document.getElementById("registerButton").style.display = "block";
+  }
+
+  review(): void {
+    // this.showReview = true;
+    // this.submitter = false;
+    document.getElementById("registerButton").style.display = "none";
+    document.getElementById("reviewContainer").style.display = "block";
+
+    console.log("Now value ", this.showReview);
   }
 
 }
