45b7ae21081cfcad67d3647183293b26a0e69376
Sumit Singh <sumit.singh@nagarro.com>
2020-04-30 15:26:02 +0530

    mistake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { UserRegFormComponent } from './user-reg-form/user-reg-form.component';
 import { UsersService } from './services/users.service';
+import { RegionsService } from './services/regions.service';
 import { UserDComponent } from './dashboards/user-d/user-d.component';
 
 @NgModule({
@@ -23,8 +25,9 @@ import { UserDComponent } from './dashboards/user-d/user-d.component';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [UsersService],
+  providers: [UsersService, RegionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user-reg-form/user-reg-form.component.ts
+++ src/app/user-reg-form/user-reg-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../services/users.service';
+import { Observable } from 'rxjs';
+import { RegionsService } from '../services/regions.service';
 
 @Component({
   selector: 'app-user-reg-form',
@@ -7,17 +11,71 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./user-reg-form.component.css']
 })
 export class UserRegFormComponent implements OnInit {
+  userForm: FormGroup;
+  validMessage: string;
 
   public showReview: boolean;
   public submitter: boolean;
+  countries: Array<any> = [];
 
-  constructor() { }
+  constructor(private userService: UsersService, private regionService: RegionsService) { }
 
   ngOnInit(): void {
     // this.showReview = false;
     // this.submitter = true;
     document.getElementById("reviewContainer").style.display = "none";
     document.getElementById("registerButton").style.display = "block";
+
+    this.regionService.getCountries().toPromise()
+      .then(i =>
+        // this.countries = Object.keys(i)
+        Object.keys(i).forEach(j => {
+          // console.log(i[j]["name"])
+          this.countries.push({ j: i[j]["names"] })
+        })
+      );
+
+    this.countries.forEach(i => {
+      console.log(i);
+    })
+    this.userForm = new FormGroup({
+      fname: new FormControl('', Validators.required),
+      lname: new FormControl('', Validators.required),
+
+      email: new FormControl('', Validators.required),
+
+      bUnit: new FormControl('', Validators.required),
+      title: new FormControl('', Validators.required),
+      telephone: new FormControl('', Validators.required),
+
+      add1: new FormControl('', Validators.required),
+      add2: new FormControl(),
+
+      country: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+
+      zip: new FormControl('', Validators.required),
+    });
+
+  }
+
+  submitUserForm() {
+
+    if (this.userForm.valid) {
+      this.validMessage = "Your registration form has been submitted. Thank You!";
+      this.userService.registerUser(this.userForm.value).subscribe(
+        data => {
+          this.userForm.disable();
+          return true;
+        },
+        error => {
+          return Observable.throw(error);
+        }
+      )
+    } else {
+      this.validMessage = "Please fill out the form correctly before submitting!";
+    }
   }
 
   review(): void {
@@ -28,5 +86,4 @@ export class UserRegFormComponent implements OnInit {
 
     console.log("Now value ", this.showReview);
   }
-
 }
